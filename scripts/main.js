1be8b4792e47467a58ec01883c80bccc83982f0f
kuba <ada@localhost.localdomain>
2019-07-03 15:40:58 +0200

    update subpages styles, add form validation

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+let sendButton, form;
+
+function handleFormChanged () {
+	sendButton.disabled = !form.checkValidity();
+}
+
+window.onload = function () {
+	sendButton = document.getElementById("send-button");
+	form = document.getElementById("contact-form");
+};
