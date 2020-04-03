17d004f00e70bf7c3d6ab49e986651cf41ca7611
rooter <adzina3@vp.pl>
2020-04-03 17:24:00 +0200

    add back to top function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'jakub-wisniowski';
+  title = "jakub-wisniowski";
+
+  @ViewChild("backToTop")
+  backToTop: ElementRef;
+
+  scrollToTop = () => {
+    const button = (this.backToTop.nativeElement as HTMLElement);
+
+    button.classList.add("active");
+    const c = document.documentElement.scrollTop || document.body.scrollTop;
+    if (c > 0) {
+      window.requestAnimationFrame(this.scrollToTop);
+      window.scrollTo(0, c - 30);
+    } else {
+      button.classList.remove("active");
+      button.blur();
+    }
+  }
 }
