ce7a06b6b7b7bf248ca726d312ced75a96d6573e
Jakub Wiśniowski <jakub.m.wisniowski@gmail.com>
2020-09-01 09:32:54 +0200

    modify projects display on homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './homepage/home/home.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ScullyLibModule } from '@scullyio/ng-lib';
 import { SharedModule } from './shared/shared.module';
+import { ProjectsOverviewComponent } from './homepage/projects-overview/projects-overview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProjectsOverviewComponent
   ],
   imports: [
     BrowserModule,
